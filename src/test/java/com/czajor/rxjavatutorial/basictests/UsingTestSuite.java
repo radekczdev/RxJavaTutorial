34d665bbeefed5011a4a98e809ee27b2e53c5d18
radekczdev <radekcz.dev@gmail.com>
2020-02-11 09:28:38 +0100

    Added tests for different possibilities of reading files with RxJava

--- src/test/java/com/czajor/rxjavatutorial/basictests/UsingTestSuite.java
+++ src/test/java/com/czajor/rxjavatutorial/basictests/UsingTestSuite.java
@@ -1,12 +1,18 @@
 package com.czajor.rxjavatutorial.basictests;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UsingTestSuite {
     private String result = "";
+    private String path = "test.txt";
+
 
     @Test
     void shouldWriteResource() {
@@ -25,5 +31,42 @@ public class UsingTestSuite {
                 e -> result += e
         );
         assertEquals("My Resource", result);
+    }
+
+    @Test
+    void shouldReadFromFile_UsingFlowableGenerate() {
+        System.out.println("--- USING FLOWABLE GENERATE TO READ FILE ---");
+        Flowable<String> file = Flowable.generate(
+                () -> new BufferedReader(new FileReader(path)),
+                (reader, emitter) -> {
+                    final String line = reader.readLine();
+                    if(line != null) {
+                        emitter.onNext(line);
+                    } else {
+                        emitter.onComplete();
+                    }
+                },
+                BufferedReader::close
+        );
+        file.subscribe(
+                line -> System.out.println(line),
+                error -> System.out.println(error)
+        );
+        System.out.println("-----------------------------------------");
+    }
+
+    @Test
+    void shouldReadFromFile_UsingObservableUsing() {
+        System.out.println("--- USING FLOWABLE USING TO READ FILE ---");
+        Observable<String> observable = Observable.using(
+                () -> new BufferedReader(new FileReader(path)),
+                reader -> Observable.fromIterable(() -> reader.lines().iterator()),
+                reader -> reader.close()
+        );
+        observable.subscribe(
+                line -> System.out.println(line),
+                error -> System.out.println(error)
+        );
+        System.out.println("-----------------------------------------");
     }
 }
